37fff7b41f87d00e3d69d22c205f1fd84eb1d8d9
nishant7929 <nishantpatel7929@gmail.com>
2025-02-01 00:40:52 +0530

    revert json file

--- telegramClient.ts
+++ telegramClient.ts
@@ -1,32 +1,17 @@
 import "dotenv/config";
-import { Api, TelegramClient } from 'telegram';
+import { Api, Logger, TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions';
-import fs from 'fs/promises';
+import { LogLevel } from 'telegram/extensions/Logger';
 
 const API_ID = process.env.API_ID || 0;
 const API_HASH = process.env.API_HASH || "";
 const savedSession = process.env.TELEGRAM_SESSION || "";
 const SESSION = new StringSession(savedSession);
-const CACHE_FILE = 'cached_messages.json';
 
 let globalTelegramClient: TelegramClient | null = null;
-
-const readCache = async (): Promise<Api.Message[]> => {
-  try {
-    const data = await fs.readFile(CACHE_FILE, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    return [];
-  }
-};
-
-const writeCache = async (messages: Api.Message[]) => {
-  try {
-    await fs.writeFile(CACHE_FILE, JSON.stringify(messages, null, 2), 'utf-8');
-  } catch (error) {
-    console.error("Error writing cache:", error);
-  }
-};
+let cachedMessages: Api.Message[] = []; // Store cached messages
+// let lastFetchedTime = 0; // Timestamp of last fetch
+// const CACHE_DURATION = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 export const getTelegramClient = async (): Promise<TelegramClient> => {
   if (!globalTelegramClient) {
@@ -34,24 +19,27 @@ export const getTelegramClient = async (): Promise<TelegramClient> => {
       connectionRetries: 5,
       useWSS: true,
     });
+
     await globalTelegramClient.connect();
+
   }
   return globalTelegramClient;
 };
 
 export const getSavedMessages = async (limit: number) => {
-  const cachedMessages = await readCache();
+  const now = Date.now();
+
+  // Check if cached data is still valid
   if (cachedMessages.length > 0) {
     console.log('Returning cached messages');
-    return { origin: 'cache', messages: limit ? cachedMessages.slice(0, limit) : cachedMessages };
+    return { origin: 'cache', messages: limit ? cachedMessages.slice(0, limit) : cachedMessages  };
   }
 
   console.log('Fetching new messages from Telegram...');
   const client = await getTelegramClient();
   const me = await client.getMe();
-  const newMessages = await client.getMessages(me.id);
-  await writeCache(newMessages);
-  // client.disconnect();
-
-  return { origin: 'telegram', messages: limit ? newMessages.slice(0, limit) : newMessages };
+  cachedMessages = await client.getMessages(me.id);
+  client.disconnect();
+  
+  return { origin: 'telegram', cachedMessages };
 };
